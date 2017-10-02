9e09a6eed7895d3d53d78bff2c36aadfdbf7abb8
Meiyo40 <loic.demesmay@gmail.com>
2017-10-02 23:35:40 +0200

    DBFIX -Possibilité de prendre la suite de la quête 2024 à Uldaman en utilisant le gob lié. -Ajout du script embryon des Initié Hédérines

---
+++ DB-Updates/Uldaman/Quest 2204 Trouver & réparer le collier.sql
@@ -0,0 +1,8 @@
+/*
+SQLyog Community v12.4.3 (64 bit)
+MySQL - 5.7.16-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `gameobject_questrelation` (`id`, `quest`) values('112877','2204');
